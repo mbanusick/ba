167c469bbeba56e60721456adbb2202a154af6d0
Sab Udeh Chukwumdimma <chukwumdimma4life@gmail.com>
2019-12-16 14:32:27 +0100

    Modify table creation table

--- createTables.php
+++ createTables.php
@@ -6,6 +6,7 @@ $dbname = "siteKnab";
 
 
 try {
+
     $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
     // set the PDO error mode to exception
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
@@ -19,61 +20,64 @@ try {
     lastname VARCHAR(50) NOT NULL,
 	password VARCHAR(255) NOT NULL,
 	email VARCHAR(50) NOT NULL UNIQUE,
-    phone INT NOT NULL,
-	dateofbirth INT NOT NULL,
+    phone CHAR NOT NULL,
+	dateofbirth DATE,
 	gender VARCHAR(10) NOT NULL,
 	address VARCHAR(50) NOT NULL,
 	country VARCHAR(25) NOT NULL,
 	state VARCHAR(50) NOT NULL,
-	zip INT NOT NULL,
+	zip CHAR NOT NULL,
 	account_type VARCHAR(15) NOT NULL,
 	account_pin INT NOT NULL,
 	date_created DATETIME DEFAULT CURRENT_TIMESTAMP
-	)");
+    )");
+    
     $conn->exec("CREATE TABLE Account (
     id INT NOT NULL PRIMARY KEY AUTO_INCREMENT, 
     user_id VARCHAR(30) NOT NULL,
-    account_number BIGINT NOT NULL UNIQUE,
+    account_number INT NOT NULL UNIQUE,
     date_created DATETIME DEFAULT CURRENT_TIMESTAMP
+    -- FOREIGN KEY (user_id) REFERENCES Users(id)
     )");
+
     $conn->exec("CREATE TABLE Balance (
-        id INT NOT NULL PRIMARY KEY AUTO_INCREMENT, 
-        account_id VARCHAR(30) NOT NULL,
-        amount INT NOT NULL,
-        date_created DATETIME DEFAULT CURRENT_TIMESTAMP
-        )");
+    id INT NOT NULL PRIMARY KEY AUTO_INCREMENT, 
+    account_id VARCHAR(30) NOT NULL,
+    amount INT NOT NULL,
+    date_created DATETIME DEFAULT CURRENT_TIMESTAMP
+    -- FOREIGN KEY (account_id) REFERENCES Account(id)
+    )");
+
 	$conn->exec("CREATE TABLE Transaction (
     id INT NOT NULL PRIMARY KEY AUTO_INCREMENT, 
     details VARCHAR(30) NOT NULL,
     amount VARCHAR(30) NOT NULL,
-	user_id INT NOT NULL,
+	account_id INT NOT NULL,
+    type TINYINT NOT NULL,
+    txid VARCHAR(100) NOT NULL,
 	date_updated DATETIME DEFAULT CURRENT_TIMESTAMP
+    -- FOREIGN KEY (account_id) REFERENCES Account(id)
     )");
+
 	$conn->exec("CREATE TABLE Log (
     id INT NOT NULL PRIMARY KEY AUTO_INCREMENT, 
     user_id INT NOT NULL,
     ip VARCHAR(55) NOT NULL,
     browser VARCHAR(255) NOT NULL,
 	date_updated DATETIME DEFAULT CURRENT_TIMESTAMP
+    -- FOREIGN KEY (user_id) REFERENCES Users(id)
     )");
-
     
     // commit the transaction
     $conn->commit();
     echo "New records created successfully";
-    }
-	
-    
-    
-	
-catch(PDOException $e)
-    {
+
+} catch(PDOException $e) {
     // roll back the transaction if something failed
     $conn->rollback();
     echo "Error: " . $e->getMessage();
-    }
-	
-
+}
 	
 $conn = null;
+
 ?>
